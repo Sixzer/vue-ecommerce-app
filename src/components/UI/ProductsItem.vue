612eb9b7370e67fc9a473ae293b952b5af63ca56
Sixzer <iam6zer@gmail.com>
2023-10-05 19:48:48 +0300

    ProductsItem style fix

--- src/components/UI/ProductsItem.vue
+++ src/components/UI/ProductsItem.vue
@@ -2,6 +2,8 @@
 import { type IProduct } from "@/assets/interfaces";
 import { RouterLink } from "vue-router";
 import { useShopStore } from "@/stores/store";
+import Rating from "primevue/rating";
+import Button from "primevue/button";
 
 const props = defineProps<{ product: IProduct }>();
 const shopStore = useShopStore();
@@ -15,135 +17,53 @@ const handleClick = () => {
 </script>
 
 <template>
-    <section class="product-card">
+    <section
+        class="card flex flex-column justify-content-between align-items-stretch p-3 m-3 border-round-sm transition-transform transition-duration-300 animation-ease-in-out shadow-4"
+    >
         <RouterLink
             :to="'/products/' + product.id"
-            class="product-item"
+            class="no-underline cursor-pointer"
             @click="handleClick"
         >
             <img
                 :src="product.image"
                 alt="Product Image"
-                class="product-image"
+                class="w-full h-15rem"
+                style="object-fit: contain"
             />
-            <h3 class="product-title">{{ product.title }}</h3>
-            <article class="product-info">
-                <p class="product-info__price">{{ product.price }} $</p>
-                <article class="product-info__rating">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="rgb(255, 191, 0)"
-                        style="width: 15px; margin-top: 5px"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                            clipRule="evenodd"
-                        />
-                    </svg>
-
-                    <span class="product-info__rating__rate"
-                        >{{ product.rating.rate }}
-                    </span>
-                    <span class="product-info__rating__count">{{
-                        product.rating.count
-                    }}</span>
-                </article>
+            <h3
+                class="text-900 font-bold mt-2 text-sm line-height-2"
+                style="text-wrap: balance"
+            >
+                {{ product.title }}
+            </h3>
+            <article
+                class="flex justify-content-between align-items-center mt-2"
+            >
+                <p
+                    class="product-info__price font-bold text-lg line-height-2 text-600"
+                >
+                    {{ product.price }} $
+                </p>
+                <Rating
+                    v-model="product.rating.rate"
+                    :cancel="false"
+                    readonly
+                />
             </article>
         </RouterLink>
-        <button @click="addItemToCart(product.id)" class="product-button">
-            Add to cart
-        </button>
+
+        <Button
+            label="Add to cart"
+            size="small"
+            @click="addItemToCart(product.id)"
+            class="mt-3"
+        />
     </section>
 </template>
 
 <style scoped>
-.product-card {
-    border: 1px solid #ddd;
-    padding: 1rem;
-    margin: 1rem;
-    background-color: #fff;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-    border-radius: 5px;
-    transition: transform 0.2s ease-in-out;
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
-    justify-content: space-between;
-}
-
-.product-card:hover {
+.card:hover {
     transform: translateY(-5px);
-}
-
-.product-item {
-    text-decoration: none;
-    cursor: pointer;
-}
-
-.product-info {
-    display: flex;
-    justify-content: space-between;
-}
-
-.product-image {
-    width: 100%;
-    height: 250px;
-    object-fit: contain;
-}
-
-.product-title {
-    margin-top: 10px;
-    font-weight: bold;
-    font-size: 14px;
-    line-height: 18px;
-    color: #333;
-    text-wrap: balance;
-}
-
-.product-info__price,
-.product-info__rating__rate,
-.product-info__rating__count {
-    margin-top: 5px;
-    font-weight: bold;
-    font-size: 14px;
-    line-height: 18px;
-    color: rgba(0, 0, 0, 0.6);
-    text-transform: uppercase;
-}
-
-.product-info__rating__rate::after {
-    content: "";
-    display: inline-block;
-    vertical-align: middle;
-    border-radius: 50%;
-    width: 4px;
-    height: 4px;
-    background-color: rgba(0, 0, 0, 0.6);
-    margin: 0 0 2px 5px;
-}
-
-.product-info__rating {
-    display: flex;
-    gap: 5px;
-}
-
-.product-button {
-    background-color: transparent;
-    color: #333;
-    border: 1px solid #333;
-    padding: 0.5rem 1rem;
-    border-radius: 5px;
-    cursor: pointer;
-    margin-top: 1rem;
-    transition: background-color 0.3s ease;
-    transition: color 0.3s ease;
-    justify-self: flex-end;
-}
-
-.product-button:hover {
-    background-color: #333;
-    color: white;
 }
 </style>
