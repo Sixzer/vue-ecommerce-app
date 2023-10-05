5636f7e28aceaf01befd4db28ab3e2b87dc3e2db
Sixzer <iam6zer@gmail.com>
2023-10-05 20:28:57 +0300

    LoginSection style fix

--- src/components/LoginSection.vue
+++ src/components/LoginSection.vue
@@ -1,83 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import InputText from "primevue/inputtext";
+import Button from "primevue/button";
+</script>
 
 <template>
-    <div class="auth-container">
-        <form class="auth-form" @submit.prevent>
+    <div class="flex justify-content-center align-items-center h-screen">
+        <form class="flex flex-column gap-3 p-3 w-3 shadow-4" @submit.prevent>
             <h2 class="auth-title">Authorization</h2>
-            <div class="form-group__username">
-                <label for="username" class="form-label">Username:</label>
-                <input
-                    type="text"
-                    id="username"
-                    class="form-input"
-                    placeholder="Enter username"
-                />
+
+            <div class="flex flex-column gap-2">
+                <label for="username">Username</label>
+                <InputText id="username" aria-describedby="username-help" />
             </div>
-            <div class="form-group__password">
-                <label for="password" class="form-label">Password:</label>
-                <input
-                    type="password"
-                    id="password"
-                    class="form-input"
-                    placeholder="Enter password"
-                />
+            <div class="flex flex-column gap-2">
+                <label for="password">Password</label>
+                <InputText id="password" aria-describedby="password-help" />
             </div>
-            <button class="auth-button">Login</button>
+
+            <Button label="Sign In" />
         </form>
     </div>
 </template>
 
-<style scoped>
-.auth-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-}
-
-.auth-form {
-    background-color: #ffffff;
-    border-radius: 8px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-    padding: 20px;
-    width: 300px;
-    text-align: center;
-}
-
-.auth-title {
-    font-size: 24px;
-    margin-bottom: 20px;
-}
-
-.form-group__password,
-.form-group__username {
-    margin-bottom: 15px;
-}
-
-.form-label {
-    font-size: 16px;
-    display: block;
-}
-
-.form-input {
-    width: 100%;
-    padding: 10px;
-    font-size: 16px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-}
-
-.auth-button {
-    background-color: #333;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    padding: 10px 20px;
-    font-size: 18px;
-    cursor: pointer;
-}
-
-.auth-button:hover {
-    background-color: gray;
-}
-</style>
+<style scoped></style>
